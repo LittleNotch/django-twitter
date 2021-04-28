2c22e668be752e62c139b0d788eeb0d57802a7d6
Shi <Shirley_Shi@alliedtelesis.com>
2021-04-28 13:59:56 -0500

    django debug tool

--- twitter/settings.py
+++ twitter/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = 'django-insecure-)h(j3ly=$d!4#wk*)=i9ba2aplu69sz=3!dwv%3ex(o9)=3t3x
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '192.168.33.10', 'localhost']
+INTERNAL_IPS = ['10.0.2.2']
 
 
 # Application definition
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'accounts',
+    'debug_toolbar',
 ]
 
 REST_FRAMEWORK = {
@@ -55,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'twitter.urls'
