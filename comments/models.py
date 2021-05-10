40a52b59a244f1c922f9dd84ab3f501e31576769
Shi <Shirley_Shi@alliedtelesis.com>
2021-05-10 14:56:57 -0500

    10-comments-model-admin

---
+++ comments/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from django.db import models
+from tweets.models import Tweet
+
+# Create your models here.
+class Comment(models.Model):
+    """
+    simple comment, on a Tweet, not recursive comment
+    """
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    tweet = models.ForeignKey(Tweet, on_delete=models.SET_NULL, null=True)
+    content = models.TextField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # sort all the comments under a tweet
+        index_together = (('tweet', 'created_at'),)
+
+    def __str__(self):
+        return '{} - {} says {} at tweet {}'.format(
+            self.created_at,
+            self.user,
+            self.content,
+            self.tweet_id,
+        )
+
