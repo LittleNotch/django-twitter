5af228c93a413bf41afd37bbaf016ce3b50282c2
Shi <Shirley_Shi@alliedtelesis.com>
2021-06-08 17:41:31 -0500

    36-denormalization-model-change

---
+++ likes/listeners.py
@@ -0,0 +1,30 @@
+def incr_likes_count(sender, instance, created, **kwargs):
+    from tweets.models import Tweet
+    from django.db.models import F
+
+    if not created:
+        return
+
+    model_class = instance.content_type.model_class()
+    if model_class != Tweet:
+        # TODO do similar operations for Comment
+        return
+
+    # Don't use tweet.likes_count += 1; tweet.save()
+    # not atomic, must use "update"
+    tweet = instance.content_object
+    Tweet.objects.filter(id=tweet.id).update(likes_count=F('likes_count') + 1)
+
+
+def decr_likes_count(sender, instance, **kwargs):
+    from tweets.models import Tweet
+    from django.db.models import F
+
+    model_class = instance.content_type.model_class()
+    if model_class != Tweet:
+        # TODO do similar operations for Comment
+        return
+
+    # handle tweet likes cancel
+    tweet = instance.content_object
+    Tweet.objects.filter(id=tweet.id).update(likes_count=F('likes_count') - 1)
