33952457aac7eecf4827faf70d5ecbe1f1e88a8b
Shi <Shirley_Shi@alliedtelesis.com>
2021-07-04 18:06:55 -0500

    41-initial_django-hbase

--- tweets/models.py
+++ tweets/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 from likes.models import Like
 from tweets.constants import TweetPhotoStatus, TWEET_PHOTO_STATUS_CHOICES
 from tweets.listeners import push_tweet_to_cache
@@ -81,5 +81,6 @@ class TweetPhoto(models.Model):
         return f'{self.tweet_id}: {self.file}'
 
 
+pre_delete.connect(invalidate_object_cache, sender=Tweet)
 post_save.connect(invalidate_object_cache, sender=Tweet)
 post_save.connect(push_tweet_to_cache, sender=Tweet)
